ad669e640eb9e7ea810bb0996b08250495f66d1b
furkankykc <furkanfbr@gmail.com>
2021-06-19 09:20:45 +0300

    new

--- main.py
+++ main.py
@@ -58,44 +58,56 @@ def metabolite(fileName):
     iterator = 0
     while iterator < numberOfTests:
         # print(i, '|', lines[i])
+
         numberOfCases = 3
         # firstRow, secondRow, thirdRow = tuple(int(x) for x in lines[i].split(' '))
         seq = []
+        massExcludedList = None
+        seqs = None
+        flattenList = None
         # print(lines[i])
 
         for j in range(i + 1, i + numberOfCases + 1):
             # print(lines[j])
             # seq.append(list(map(lambda x: float(x), lines[j].split(' '))))
-            seq.append(np.fromstring(linecache.getline(fN, j), dtype=float, sep=' '))
+            seq.append(np.fromstring(linecache.getline(fN, j), dtype=np.float64, sep=' '))
         print("Data Reading Progress Finished")
         i = i + numberOfCases + 1
         iterator += 1
         # print(len(seq), len(seq[0]))
-        filteredList = np.array([seq[1] + x for x in seq[0]], dtype=np.float32)
-
+        filteredList = np.array([seq[1] + x for x in seq[0]], dtype=np.float64)
+        seqs = seq[2]
+        del seq
         # filteredList = np.where(temp > 0, temp, float('inf'))
         shp = filteredList.shape
-        filteredList.ravel()
-        # del filteredList
+        flattenList = filteredList.flatten()
+        del filteredList
 
         # with open('flattedList.pickle', 'wb') as handle:
         #     pickle.dump(flattedList, handle, protocol=pickle.HIGHEST_PROTOCOL)
         # return
-        enumeratedList = {k: v for k, v in enumerate(filteredList)}
-        del filteredList
-        sortedList = dict(sorted(enumeratedList.items(), key=lambda item: item[1]))
-        del enumeratedList
-        keys = np.fromiter(sortedList.keys(), dtype=int)
-        vals = np.fromiter(sortedList.values(), dtype=float)
-        del sortedList
+        # enumeratedList = {k: v for k, v in enumerate(flattenList)}
+        # del flattenList
+        # sortedList = dict(sorted(enumeratedList.items(), key=lambda item: item[1]))
+        # del enumeratedList
+        # keys = np.fromiter(sortedList.keys(), dtype=int)
+        # vals = np.fromiter(sortedList.values(), dtype=float)
+        # vals = np.abs(vals)
+
+        # del sortedList
         # massExcludedList
         print("Aggregation Progress Finished")
 
-        for mof in tqdm(np.nditer(seq[2])):  # masses of signal
-            # massExcludedList = np.abs(filteredList - mof)
+        for mof in tqdm(seqs):  # masses of signal
+            massExcludedList= None
+            massExcludedList = np.abs(flattenList - mof)
 
-            key = keys[np.argmax(vals >= mof)]
-            indexes = np.unravel_index(key, shp)
+            # key = keys[np.argmax(vals >= mof)
+            # key = np.argwhere(massExcludedList ==np.min(massExcludedList))
+            # reduced = vals[key:key+10000]
+            # key = keys[np.argmax(reduced >= mof)]
+            indexes = np.unravel_index(massExcludedList.argmin(), shp)
+            # print(key, mof)
             out += f"{' '.join(str(v + 1) for v in indexes)}\n"
             # for k, v in sortedList.items():
             #     if v >= mof:
@@ -167,6 +179,8 @@ def metabolite2(fileName):
     f = open(f'res2/res{fileName}', 'w')
     f.write(out)  # python will convert \n to os.linesep
     f.close()  #
+
+
 def metabolite3(fileName):
     out = ''
     with open(f'data2/{fileName}') as file:
@@ -213,6 +227,46 @@ def metabolite3(fileName):
     f.close()  #
 
 
+def metabolite4(fileName):
+    out = ''
+    with open(f'data2/{fileName}') as file:
+        lines = [line.rstrip() for line in file]
+        numberOfTests = int(lines[0])
+        i = 1
+        iterator = 0
+        while iterator < numberOfTests:
+            # print(i, '|', lines[i])
+            numberOfCases = 3
+            # firstRow, secondRow, thirdRow = tuple(int(x) for x in lines[i].split(' '))
+            seq = []
+            # print(lines[i])
+            for j in range(i + 1, i + numberOfCases + 1):
+                # print(lines[j])
+                # seq.append(list(map(lambda x: float(x), lines[j].split(' '))))
+                seq.append(np.fromstring(lines[j], dtype=np.float64, sep=' '))
+
+            i = i + numberOfCases + 1
+            iterator += 1
+            print("Data Reading Progress Finished")
+
+            afreq = (np.max(seq[0]) - np.min(seq[0])) * len(seq[0])
+            mfreq = (np.max(seq[1]) - np.min(seq[1])) * len(seq[1])
+            aList = dict(sorted(enumerate(seq[0]), key=lambda item: item[1]))
+            mList = dict(sorted(enumerate(seq[1]), key=lambda item: item[1]))
+            print(afreq, mfreq)
+            for inn in seq[2]:
+                m = inn * mfreq
+                valM = aList[int(m)]
+                a = (valM - inn) * afreq
+                valA = mList[int(a)]
+                print(inn, inn - (valM + valA), int(a), int(m))
+                out += f"{int(a) + 1}{int(m) + 1}\n"
+            print("Result Checking Progress Finished")
+            f = open(f'res2/res{fileName}', 'w')
+            f.write(out)  # python will convert \n to os.linesep
+            f.close()  #
+
+
 def readFromPickle():
     with open('flattedList.pickle', 'rb') as handle:
         b = pickle.load(handle)
@@ -228,7 +282,7 @@ if __name__ == '__main__':
 
     start = time.time()
     # for i in range(1, 2):
-    metabolite2('4.txt')
+    metabolite('4.txt')
     # readFromPickle()
     end = time.time()
     print((end - start))
